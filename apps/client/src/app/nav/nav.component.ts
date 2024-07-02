dd208a8c21b30868eb23ccbb01cd307ea256c3b2
Clarence Adrian <44561376+ad2969@users.noreply.github.com>
2024-07-01 18:47:39 -0700

    feat(client): basic navigation

---
+++ apps/client/src/app/nav/nav.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+
+@Component({
+  selector: 'fse-nav',
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule,
+    AsyncPipe,
+  ]
+})
+export class NavComponent {}
