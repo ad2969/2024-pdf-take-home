5358275558e90ba0c4f84c60eb839f18840f46e9
Clarence Adrian <44561376+ad2969@users.noreply.github.com>
2024-07-06 00:49:53 -0700

    fix(client): add explicit api url in http calls

--- apps/client/src/app/api/api.service.ts
+++ apps/client/src/app/api/api.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { OHCLVT, News, Stock } from "@2024-pdf-take-home/domain";
 
+const API_URL = process.env['NGX_API_URL'] || 'http://localhost:3000';
+
 @Injectable({
     providedIn: 'root',
 })
@@ -10,30 +12,30 @@ export class ApiService {
     private readonly http = inject(HttpClient);
 
     lookupStocksByTicker(query: string): Observable<Stock[]> {
-        return this.http.get<Stock[]>(`/api/stocks/${query}`);
+        return this.http.get<Stock[]>(`${API_URL}/api/stocks/${query}`);
     }
 
     getStockData(ticker: string): Observable<Stock> {
-        return this.http.get<Stock>(`/api/stocks/${ticker}/detail`);
+        return this.http.get<Stock>(`${API_URL}/api/stocks/${ticker}/detail`);
     }
 
     getStockOHLCVTData(ticker: string, from: number, to: number): Observable<OHCLVT[]> {
-        return this.http.get<OHCLVT[]>(`/api/stocks/${ticker}/data/${from}/${to}`);
+        return this.http.get<OHCLVT[]>(`${API_URL}/api/stocks/${ticker}/data/${from}/${to}`);
     }
 
     getStockNewsData(ticker: string): Observable<News[]> {
-        return this.http.get<News[]>(`/api/stocks/${ticker}/news`);
+        return this.http.get<News[]>(`${API_URL}/api/stocks/${ticker}/news`);
     }
  
     getAllFavoriteStocks(): Observable<Stock[]> {
-        return this.http.get<Stock[]>(`/api/favorites`);
+        return this.http.get<Stock[]>(`${API_URL}/api/favorites`);
     }
     
     saveFavoriteStock(stockId: string): Observable<unknown> {
-        return this.http.post(`/api/favorites`, stockId);
+        return this.http.post(`${API_URL}/api/favorites`, stockId);
     }
     
     removeFavoriteStock(stockId: string): Observable<unknown> {
-        return this.http.delete(`/api/favorites/${stockId}`);
+        return this.http.delete(`${API_URL}/api/favorites/${stockId}`);
     }
 }
